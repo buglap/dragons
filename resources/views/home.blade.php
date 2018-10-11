0f61ebc30b12901b0d80e0c1ebbacf557adee733
buglap <hidej26@gmail.com>
2018-10-10 22:05:36 -0500

    some changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    I'm just learning to use git!
                 </div>
             </div>
         </div>
---
+++ resources/views/request.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Solicitud a nombre de: {{$user->name}}</div>
+                
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    the fucking git prove!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
